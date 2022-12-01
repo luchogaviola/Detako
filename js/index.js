ed370dca5411283466326b8197cad3b6e638518a
Luciano Gaviola <luchogaviola02@gmail.com>
2022-12-01 10:39:01 -0300

    hola

--- js/index.js
+++ js/index.js
@@ -15,91 +15,106 @@ const horariosDisponiblesPadel = [
         horarioInicio:'9',
         horarioFinal: '10',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 1
     },
     {
         horarioInicio:'10',
         horarioFinal: '11',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 2
     },
     {
         horarioInicio:'11',
         horarioFinal: '12',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 3
     },
     {
         horarioInicio:'12',
         horarioFinal: '13',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 4
     },
     {
         horarioInicio:'13',
         horarioFinal: '14',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 5
     },
     {
         horarioInicio:'14',
         horarioFinal: '15',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 6
     },
     {
         horarioInicio:'15',
         horarioFinal: '16',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 7
     },
     {
         horarioInicio:'16',
         horarioFinal: '17',
         disponibilidad: true,
-        cancha: 'padel'
+        cancha: 'padel',
+        id: 8
     },
     {
         horarioInicio:'17',
         horarioFinal: '18',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 9
     },
     {
         horarioInicio:'18',
         horarioFinal: '19',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 10
     },
     {
         horarioInicio:'19',
         horarioFinal: '20',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 11
     },
     {
         horarioInicio:'20',
         horarioFinal: '21',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 12
     },
     {
         horarioInicio:'21',
         horarioFinal: '22',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 13
     },
     {
         horarioInicio:'22',
         horarioFinal: '23',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 14
     },
     {
         horarioInicio:'23',
         horarioFinal: '00:00',
         disponibilidad: true,
-        cancha: "padel"
+        cancha: "padel",
+        id: 15
     }
 ]
 
@@ -108,91 +123,106 @@ const horariosDisponiblesFutbol = [
         horarioInicio:'9',
         horarioFinal: '10',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 16
     },
     {
         horarioInicio:'10',
         horarioFinal: '11',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 17
     },
     {
         horarioInicio:'11',
         horarioFinal: '12',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 18
     },
     {
         horarioInicio:'12',
         horarioFinal: '13',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 19
     },
     {
         horarioInicio:'13',
         horarioFinal: '14',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 20
     },
     {
         horarioInicio:'14',
         horarioFinal: '15',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 21
     },
     {
         horarioInicio:'15',
         horarioFinal: '16',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 22
     },
     {
         horarioInicio:'16',
         horarioFinal: '17',
         disponibilidad: true,
-        cancha: 'futbol'
+        cancha: 'futbol',
+        id: 23
     },
     {
         horarioInicio:'17',
         horarioFinal: '18',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 24
     },
     {
         horarioInicio:'18',
         horarioFinal: '19',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 25
     },
     {
         horarioInicio:'19',
         horarioFinal: '20',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 26
     },
     {
         horarioInicio:'20',
         horarioFinal: '21',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 27
     },
     {
         horarioInicio:'21',
         horarioFinal: '22',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 28
     },
     {
         horarioInicio:'22',
         horarioFinal: '23',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 29
     },
     {
         horarioInicio:'23',
         horarioFinal: '00:00',
         disponibilidad: true,
-        cancha: "futbol"
+        cancha: "futbol",
+        id: 30
     }
 ]
 
@@ -205,7 +235,7 @@ const mostrarHorarios = (element, arr) => {
         document.querySelector(element).innerHTML += 
         `
         <div class="disponible">
-            <p class="horariosIndividuales">${arr[i].horarioInicio}hs ${arr[i].horarioFinal}hs</p> <button class="botonReservar" id="botonReservar">Reservar</button>
+            <p class="horariosIndividuales">${arr[i].horarioInicio}hs ${arr[i].horarioFinal}hs</p> <button value = "${arr[i].cancha}" id = "${arr[i].id}" class='botonReservar' onclick = "abreModal(${arr[i].id})" onclick = "abreModal(${arr[i].cancha})" id="botonReservar">Reservar</button>
         </div>
         `
         ;
@@ -218,10 +248,17 @@ mostrarHorarios("#containerTurnosFutbol",horariosDisponiblesFutbol);
 
 // MOSTRAR RESERVAR TURNO? -------
 
-const botonReservar = document.querySelector("#botonReservar");
+const botonReservar = document.querySelector("button");
 const modalCentro = document.querySelector("#modal-centro");
 const cerrarModalCentro = document.querySelector("#cerrar-modal-centro");
-const botonesReserva = document.querySelectorAll(".botonesReserva")
+
+
+const abreModal = (id, value) => {
+    localStorage.setItem('id',id)
+    localStorage.setItem('value',value)
+    modalCentro.classList.add("active");
+}
+
 
 botonReservar.addEventListener("click", () => {
     modalCentro.classList.add("active");
@@ -231,36 +268,26 @@ cerrarModalCentro.addEventListener("click", () => {
     modalCentro.classList.remove("active");
 })
 
-// BOTON EN ROJO --------
 
+const botonReservarCancha = document.querySelector("#botonReservarCancha");
 
-const botonReservar2 = document.querySelectorAll("#botonReservar");
+botonReservarCancha.addEventListener("click", () => {
+    const id = localStorage.getItem('id')
+    const value = localStorage.getItem('value')
 
-botonReservar2.forEach(boton => {
-    boton.addEventListener("click", () => {
-        
-        
-
-
-        // horariosDisponiblesPadel[0].disponibilidad = false
-        // if (horariosDisponiblesPadel[0].disponibilidad === false) {
-        //     const botonReservado = document.querySelector(".botonReservar");
-        //     botonReservado.classList.add("active");
-        //     botonReservado.innerHTML = 'Reservado';
-        //     modalCentro.classList.remove("active");
-        // }
-    })
+    if (value == 'futbol') {
+        horariosDisponiblesFutbol[id].disponibilidad = false
+    if (horariosDisponiblesFutbol[id].disponibilidad === false) {
+        const botonReservado = document.getElementById(id);
+        botonReservado.classList.add("active");
+        botonReservado.innerHTML = 'Reservado';
+    }} else {
+        horariosDisponiblesPadel[id].disponibilidad = false
+        if (horariosDisponiblesPadel[id].disponibilidad === false) {
+            const botonReservado = document.getElementById(id);
+            botonReservado.classList.add("active");
+            botonReservado.innerHTML = 'Reservado';
+        }
+    }
 })
 
-
-
-// const botonReservarCancha = document.querySelector("#botonReservarCancha");
-// botonReservarCancha.addEventListener("click", () => {
-    // horariosDisponiblesPadel[0].disponibilidad = false
-    // if (horariosDisponiblesPadel[0].disponibilidad === false) {
-    //     const botonReservado = document.querySelector(".botonReservar");
-    //     botonReservado.classList.add("active");
-    //     botonReservado.innerHTML = 'Reservado';
-    //     modalCentro.classList.remove("active");
-    // }
-// })
